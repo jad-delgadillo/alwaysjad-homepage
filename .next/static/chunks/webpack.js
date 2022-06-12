60daf9695b2f85f905682ce23bd6af69673bc817
jad-delgadillo <59192230+jad-delgadillo@users.noreply.github.com>
2022-06-12 13:52:58 -0500

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9cc7a9729ec3e432"; }
+/******/ 		__webpack_require__.h = function() { return "667ecaafbc49b36d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/main.tsx
+++ components/main.tsx
@@ -72,8 +72,8 @@ const Main = () => {
                             <div className="p-4"></div>
                                 <h1 className=" text-md underline-offset-4 underline font-semibold decoration-2 mb-2">On the web</h1>
                                     <div className="flex flex-col font-medium justify-center items-start">
-                                        <button className=" flex justify-center items-center border text-cyan-300 hover:bg-cyan-800 px-2 rounded-lg">
-                                            <div className="block items-center justify-center w-[3vw]">
+                                        <button className=" flex justify-between border text-cyan-300 hover:bg-cyan-800 px-2 rounded-lg">
+                                            <div className="flex items-center justify-center w-[15px] mr-3">
                                                 <Image
                                                 src={"/assets/github.png"}
                                                 alt="logo"
