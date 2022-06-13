e27f4f077090d36b43473f5632609d43e5ebfbbd
jad-delgadillo <59192230+jad-delgadillo@users.noreply.github.com>
2022-06-13 13:04:47 -0500

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "667ecaafbc49b36d"; }
+/******/ 		__webpack_require__.h = function() { return "111e1353fe944b93"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import NavBar from '../components/navBar'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, }: AppProps) {
+  return (
+    <Component {...pageProps}>
+
+        <NavBar/>
+
+      </Component>
+
+    ) 
+
+    
 }
 
 export default MyApp
